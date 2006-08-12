ad8e52c9e34ae3f78ef616d4f1febe38e744a595
kmatsui <kmatsui@2a5df580-6312-0410-8f23-c0e9f4323ddb>
2006-08-12 08:37:24 +0000

    V.2.6.1

---
+++ V.2.6.1-prerelease/noconfig/visualc.mak
@@ -0,0 +1,103 @@
+# makefile to compile MCPP version 2.6 for Visual C / nmake
+#		2006/08	kmatsui
+# You must first edit BINDIR according to your system.
+# To make stand-alone-build of MCPP do:
+#		nmake
+# To make Visual-C-specific-build of MCPP do:
+#		nmake COMPILER=MSC
+# To re-compile MCPP using Visual-C-specific-build of MCPP do:
+#		nmake COMPILER=MSC PREPROCESSED=1
+# To link kmmalloc V.2.5.1 (malloc() package of kmatsui) or later do:
+#   (Note: Visual C 2005 cannot coexist with kmmalloc)
+#		nmake [PREPROCESSED=1] KMMALLOC=1
+# To compile MCPP with C++, rename *.c other than lib.c to *.cpp and do:
+#		nmake CPLUS=1
+
+NAME = mcpp
+
+CC = cl
+CFLAGS = $(CFLAGS) -Za -c   # -Zi
+	# Add -Zi for debugging on Visual C / IDE
+LINKFLAGS = -Fe$(NAME)  # -Zi
+CPPFLAGS = $(CPPFLAGS) -D_CRT_SECURE_NO_DEPRECATE
+	# -D_CRT_SECURE_NO_DEPRECATE for Visual C 2005
+
+!if "$(COMPILER)"=="MSC"
+CPPFLAGS = $(CPPFLAGS) -DCOMPILER=MSC
+# BINDIR : Adjust to your system.
+#	for Visual C 2003
+#BINDIR = "$(MSVCDIR)"\bin
+#	for Visual C 2005
+BINDIR = "$(VCINSTALLDIR)"\bin
+!else
+# stand-alone-build: use compiler-independent directory
+BINDIR = \PUBLIC\bin
+!endif
+
+!ifdef CPLUS
+LANG = -TP
+preproc = preproc.cpp
+!else
+LANG = -TC
+preproc = preproc.c
+!endif
+
+!ifdef KMMALLOC
+MEM_MACRO = -DKMMALLOC -D_MEM_DEBUG -DXMALLOC
+MEMLIB = kmmalloc_debug.lib
+!else
+MEM_MACRO =
+MEMLIB =
+!endif
+
+OBJS = main.obj control.obj eval.obj expand.obj support.obj system.obj	\
+	mbchar.obj lib.obj
+
+$(NAME).exe : $(OBJS)
+	$(CC) $(LINKFLAGS) $(OBJS) $(MEMLIB)
+#install :
+# Visual C++ / IDE "makefile project" does not have "make install" command
+	copy /b $(NAME).exe $(BINDIR)\$(NAME).exe
+
+!ifdef PREPROCESSED
+# make a "pre-preprocessed" header file to recompile MCPP with MCPP.
+mcpp.H	: system.H internal.H
+	$(NAME) $(CPPFLAGS) $(LANG) $(MEM_MACRO) $(preproc) mcpp.H
+$(OBJS) : mcpp.H
+system.H	: noconfig.H
+!else
+$(OBJS) : noconfig.H
+main.obj control.obj eval.obj expand.obj support.obj system.obj mbchar.obj:	\
+        system.H internal.H
+!endif
+
+!ifdef PREPROCESSED
+!ifdef CPLUS
+.cpp.obj:
+	$(NAME) -DPREPROCESSED $(LANG) $< $(<B).i
+	$(CC) $(CFLAGS) $(LANG) $(<B).i
+.c.obj	:
+	$(NAME) $(CPPFLAGS) $< $(<B).i
+	$(CC) $(CFLAGS) -TC $(<B).i
+!else
+.c.obj	:
+	$(NAME) -DPREPROCESSED $(CPPFLAGS) $< $(<B).i
+	$(CC) $(CFLAGS) $(LANG) $(<B).i
+!endif
+!else
+!ifdef CPLUS
+.cpp.obj:
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(MEM_MACRO) $(LANG) $<
+.c.obj	:
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(MEM_MACRO) -TC $<
+!else
+.c.obj	:
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(MEM_MACRO) $(LANG) $<
+!endif
+!endif
+
+clean	:
+	-del *.obj
+	-del *.i
+	-del mcpp.H
+	-del $(NAME).exe
---
+++ V.2.6.1-prerelease/src/unset_mcpp.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+# script to set GNU CPP or CC1, CC1PLUS to be called from gcc
+# ./unset_mcpp.sh ${gcc_path} ${gcc_maj_ver} ${gcc_min_ver} ${cpp_call}     \
+#       ${CC} ${CXX} x${EXEEXT} ${LN_S} ${inc_dir}
+
+gcc_maj_ver=$2
+gcc_min_ver=$3
+cpp_call=$4
+CC=$5
+CXX=$6
+LN_S=$8
+inc_dir=$9
+cpp_name=`echo ${cpp_call} | sed 's,.*/,,'`
+cpp_path=`echo ${cpp_call} | sed "s,/${cpp_name},,"`
+gcc_path=`echo $1 | sed "s,/${CC}\$,,"`
+EXEEXT=`echo $7 | sed 's/^x//'`
+if test x${EXEEXT} != x; then
+    cpp_base=`echo ${cpp_name} | sed "s/${EXEEXT}//"`
+else
+    cpp_base=${cpp_name}
+fi
+sys_mingw=`echo $1 | grep 'mingw'`
+sys_cygwin=`echo ${inc_dir} | grep 'cygwin'`
+
+echo "  rm -f ${inc_dir}/mcpp_g*_predef_*.h"
+rm -f ${inc_dir}/mcpp_g*_predef_*.h
+if test ${sys_cygwin}; then
+    echo "  rm -f ${inc_dir}/mingw/mcpp_g*_predef_*.h"
+    rm -f ${inc_dir}/mingw/mcpp_g*_predef_*.h
+fi
+
+echo "  cd ${cpp_path}"
+cd ${cpp_path}
+
+sym_link=`ls -l ${cpp_name} | sed 's/^l.*/l/; s/^[^l].*//'`;
+if (test x${sym_link} = xl || test ${sys_mingw})  \
+        && test -f ${cpp_base}_gnuc${EXEEXT}; then
+    rm -f ${cpp_name} mcpp.sh
+    echo "  mv ${cpp_base}_gnuc${EXEEXT} ${cpp_name}"
+    mv -f ${cpp_base}_gnuc${EXEEXT} ${cpp_name}
+    if test x${cpp_base} = xcc1; then
+        rm -f cc1plus${EXEEXT} mcpp_plus.sh
+        echo "  mv cc1plus_gnuc${EXEEXT} cc1plus${EXEEXT}"
+        mv -f cc1plus_gnuc${EXEEXT} cc1plus${EXEEXT}
+    fi
+fi
+
+if test x${gcc_maj_ver} = x2; then
+    exit 0
+fi
+
+echo "  cd ${gcc_path}"
+cd ${gcc_path}
+if test ! ${sys_mingw}; then
+    c_sh=`readlink ${CC}`
+fi
+if test x${c_sh} = x${CC}.sh || test ${sys_mingw}; then
+    if test `grep _proper ${CC}.sh > /dev/null; echo $? = 0`; then
+        c_entity=`grep ${gcc_path} ${CC}.sh | sed "s,${gcc_path}/,,"  \
+                | sed 's/_proper.*$//'`
+        rm -f ${c_entity}
+        echo "  mv ${c_entity}_proper${EXEEXT} ${c_entity}${EXEEXT}"
+        mv -f ${c_entity}_proper${EXEEXT} ${c_entity}${EXEEXT}
+    else
+        c_entity=`grep integrated ${CC}.sh | sed "s, .*,,"`
+        echo "  ${LN_S} ${c_entity} ${CC}"
+        ${LN_S} -f ${c_entity} ${CC}
+    fi
+    rm -f ${CC}.sh
+fi
+if test ! ${sys_mingw}; then
+    cxx_sh=`readlink ${CXX}`
+fi
+if test x${cxx_sh} = x${CXX}.sh || test ${sys_mingw}; then
+    if test `cat ${CXX}.sh | grep _proper > /dev/null; echo $? = 0`; then
+        cxx_entity=`grep ${gcc_path} ${CXX}.sh | sed "s,${gcc_path}/,,"  \
+                | sed 's/_proper.*$//'`
+        rm -f ${cxx_entity}
+        echo "  mv ${cxx_entity}_proper${EXEEXT} ${cxx_entity}${EXEEXT}"
+        mv -f ${cxx_entity}_proper${EXEEXT} ${cxx_entity}${EXEEXT}
+    else
+        cxx_entity=`grep integrated ${CXX}.sh | sed "s, .*,,"`
+        echo "  ${LN_S} ${cxx_entity} ${CXX}"
+        ${LN_S} -f ${cxx_entity} ${CXX}
+    fi
+    rm -f ${CXX}.sh
+fi
